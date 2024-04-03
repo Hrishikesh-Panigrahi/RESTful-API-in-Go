84a8b326416d43b5774fa5d23afa0ec5c12fa951
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2024-04-03 11:15:33 +0530

    something

---
+++ connectors/Register.go
@@ -0,0 +1,61 @@
+package connectors
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Hrishikesh-Panigrahi/RESTful-API-in-Go/models"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/Hrishikesh-Panigrahi/RESTful-API-in-Go/initializers"
+)
+
+func Register(c *gin.Context) {
+	type body struct {
+		Email    string `json:"Email"`
+		Password string `json:"password"`
+		Name     string `json:"name"` // A regular string field
+		Role     string `json:"role"` // A regular string field
+	}
+
+	var b body
+
+	if c.Bind(&b) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "failed to read body",
+		})
+		return
+	}
+
+	// password hash
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(b.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "failed to hash password",
+		})
+		return
+	}
+
+	//create the user
+	var user = &models.User{Email: b.Email, Password: string(passwordHash), Name: b.Name, Role: b.Role}
+
+	fmt.Print((user))
+	result := initializers.DB.Create(&user)
+
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+
+			"message": "failed to create user",
+		})
+
+		return
+	}
+
+	// code to register a user
+	fmt.Print("Registering a user...")
+
+	c.JSON(http.StatusOK, gin.H{})
+}
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	// "net/http"
 
-	"github.com/gin-gonic/gin"
-	// "github.com/Hrishikesh-Panigrahi/RESTful-API-in-Go/connectors"
+	// "net/http"
+
+	"github.com/Hrishikesh-Panigrahi/RESTful-API-in-Go/connectors"
 	"github.com/Hrishikesh-Panigrahi/RESTful-API-in-Go/initializers"
+	"github.com/gin-gonic/gin"
 	// "github.com/Hrishikesh-Panigrahi/RESTful-API-in-Go/middleware"
 )
 
@@ -15,9 +17,29 @@ func init() {
 	initializers.SyncDb()
 }
 
+// func Hello(c *gin.Context)
+// {
+
+// c.JSON({
+
+// "msg":"hello"
+
+// })
+
+// }
+
 func main() {
 	router := gin.Default()
-	// router.POST("/register", connectors.Register)
+	router.GET("/", func(c *gin.Context) {
+
+		// Call the HTML method of the Context to render a template
+		c.JSON(201, gin.H{
+			"msg": "hello",
+		})
+
+	})
+
+	router.POST("/register", connectors.Register)
 	// router.POST("/login", connectors.Login)
 
 	router.Run()
